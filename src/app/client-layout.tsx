c28d8356244c561577afcabb8656b8ebaf6048d3
MadFox <madfox@laserflare.net>
2025-04-06 14:15:40 +0400

    feat: rewrite trpc provider

--- src/app/client-layout.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { trpc } from "./utils/trpc";
-
-function ClientLayout({ children }: { children: React.ReactNode }) {
-  return children;
-}
-
-export default trpc.withTRPC(ClientLayout);
